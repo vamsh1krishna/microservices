55ec791405904ea198d18799d73cdbacdbd93b22
vamsh1krishna <vamshikrishna.ramuni@protonmail.com>
2022-01-12 20:54:30 +0530

    hello_api

---
+++ hello_api/app.py
@@ -0,0 +1,15 @@
+from flask import Flask , request, jsonify
+from models import recipes
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_all_recipes():
+    """
+    This method will return all the recipes
+    :return: Json data with all the recipes
+    """
+    return jsonify(data=recipes)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081, debug=True)
